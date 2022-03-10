c6fccc3265f02258b71caf7a4d02e2f836bec77a
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-03-11 02:06:50 +0400

    perfect

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,19 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 const Popular = () => {
+  const [popular, setPopular] = useState([]);
+
   useEffect(() => {
     getPopular();
   }, []);
 
   const getPopular = async () => {
     const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}$number=9`
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     );
     const data = await api.json();
-    console.log(data);
+    setPopular(data.recipes);
+    console.log(data.recipes);
   };
 
-  return <div>Popular</div>;
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <Wrapper key={recipe.id}>
+            <h3>Popular Picks</h3>
+            {popular.map((recipe) => {
+              return (
+                <Card key={recipe.id}>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              );
+            })}
+          </Wrapper>
+        );
+      })}
+    </div>
+  );
 };
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img {
+    border-radius: 2rem;
+  }
+`;
 
 export default Popular;

1dc78ad2a04f2340cb6901f19f6ebf8a7a7ad029
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-03-11 23:36:54 +0400

    splide, spliderslider

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -21,21 +21,30 @@ const Popular = () => {
 
   return (
     <div>
-      {popular.map((recipe) => {
-        return (
-          <Wrapper key={recipe.id}>
-            <h3>Popular Picks</h3>
-            {popular.map((recipe) => {
-              return (
-                <Card key={recipe.id}>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
                   <p>{recipe.title}</p>
                   <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
                 </Card>
-              );
-            })}
-          </Wrapper>
-        );
-      })}
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
     </div>
   );
 };
@@ -48,10 +57,40 @@ const Card = styled.div`
   min-height: 25rem;
   border-radius: 2rem;
   overflow: hidden;
+  positon: relative;
 
   img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
 
 export default Popular;

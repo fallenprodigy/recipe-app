0478eae4347f93ddc718cc1911ce1ad1e69e33c7
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-03-13 01:33:29 +0400

    c

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Home from "./Home";
+import Cuisine from "./Cuisine";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const Pages = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine" element={<Cuisine />} />
+      </Routes>
+    </Router>
   );
 };
 

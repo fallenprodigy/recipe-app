7784d8647427f0cc50fe1cb075d06c5913c51286
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-03-13 23:22:12 +0400

    routing done

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const Pages = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cuisine" element={<Cuisine />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cuisine/:type" element={<Cuisine />} />
+    </Routes>
   );
 };
 

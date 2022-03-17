fb7c11b4a61ca11e6a7dd8bd45d71bc5c655bedf
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-03-17 21:13:03 +0400

    render html tags which are inside of text

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -44,7 +44,9 @@ function Recipe() {
           Ingredients
         </Button>
         <div>
-          <h3>{details.summary}</h3>
+          <h3 dangerouslySetInnerHTML={{ __html: details.summary }}>
+            {details.summary}
+          </h3>
         </div>
       </Info>
     </DetailWrapper>
